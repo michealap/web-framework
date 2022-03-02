8d0692541202ee06b664be4e7af4d40b6c12e9d7
Micheala Palmer <michealapalmer@gmail.com>
2022-03-02 01:43:10 -0500

    add fetch functionality

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,4 @@
-import { User } from './models/User';
+import { User } from './models/User'
 
-const user = new User({name: 'Alex', age: 20});
-
-user.on('change', () => {
-  console.log("Change #1");
-});
-user.on('change', () => {
-  console.log("Change #2");
-});
-user.on('save', () => {
-  console.log("Save was triggered");
-})
-
-user.trigger('save');
+const user = new User({ id: 1 });
+user.fetch();
