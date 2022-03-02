22319577023cc052fbd69cb221c11fa10aec71a6
Micheala Palmer <michealapalmer@gmail.com>
2022-03-02 01:08:10 -0500

    implement listener support

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import { User } from './models/User';
 
 const user = new User({name: 'Alex', age: 20});
 
-user.set({ age: 35 })
-console.log(user.get('name'));
-console.log(user.get('age'));
+user.on('change', () => {
+  console.log("Change #1");
+});
+user.on('change', () => {
+  console.log("Change #2");
+});
+user.on('save', () => {
+  console.log("Save was triggered");
+})
+
+user.trigger('save');
